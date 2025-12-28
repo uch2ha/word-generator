c7e2411ff65635be7658b0c7e9b9b0226b95d307
Dmitry Sinyavskiy <sinyavskiy.d@gmail.com>
2025-12-28 20:29:55 +0200

    Main class added STAT log logic

--- src/main/java/uch2ha/Main.java
+++ src/main/java/uch2ha/Main.java
@@ -15,41 +15,68 @@ public class Main {
 
 	private static final Logger logger = LogManager.getLogger(Main.class.getName());
 
-	public static String outputFolder;
 	public static String initTime = TimestampUtil.getTimestamp();
-	public static boolean isCalculationMode = false;
+	public static Path outputFolderPath;
+	public static boolean isCalculationMode = true;
+	public static boolean isLogDebugRAM = false;
+
+	private static long totalRuWordsAmountForCalculation = 0;
+	private static long totalEnWordsAmountForCalculation = 0;
 
 	public static void main(String[] args) throws Exception {
 
 		if (args.length == 0) {
 			// Default for testing
-			args = new String[]{"configs/example.json", "output"};
+			args = new String[]{"myConfigs/example.json", "output"};
 		}
 
 		if (args.length < 2) {
-			System.err.println("Usage: java WordGenerator <config.json> <output-folder>");
+			logger.error("Usage: java WordGenerator <config.json> <output-folder>");
 			System.exit(1);
 		}
 
 		String configPath = args[0];
-		outputFolder = args[1];
+		String outputFolder = args[1];
 
-		Files.createDirectories(Path.of(outputFolder));
+		outputFolderPath = Files.createDirectories(Path.of(outputFolder));
 
 		// Parse config from JSON
 		ObjectMapper mapper = new ObjectMapper();
 		GeneratorConfig config = mapper.readValue(new File(configPath), GeneratorConfig.class);
 
-		logger.info("Starting word generator for config {}...", config.getName());
-
-		logger.info("DMITRY 1 {}", config.getCoreRu());
-		logger.info("DMITRY 2 {}", config.getCoreSplitSymbols());
-		logger.info("DMITRY 3 {}", config.isGenerateEnAsRu());
+		logger.info("Starting word generator for config {} (save words: {})...", config.getName(),
+				!isCalculationMode);
 
 		BaseWordService baseWordService = BaseWordService.getInstance();
 		baseWordService.generateAndSave(config);
 
 		logger.info("Generation complete!");
+		logCalculationStats();
 		System.exit(0);
+	}
+
+	public static long getTotalRuWordsAmountForCalculation() {
+		return totalRuWordsAmountForCalculation;
+	}
+
+	public static void setTotalRuWordsAmountForCalculation(long totalRuWordsAmountForCalculation) {
+		Main.totalRuWordsAmountForCalculation = totalRuWordsAmountForCalculation;
+	}
+
+	public static long getTotalEnWordsAmountForCalculation() {
+		return totalEnWordsAmountForCalculation;
+	}
+
+	public static void setTotalEnWordsAmountForCalculation(long totalEnWordsAmountForCalculation) {
+		Main.totalEnWordsAmountForCalculation = totalEnWordsAmountForCalculation;
+	}
+
+	private static void logCalculationStats() {
+		long totalWords = totalEnWordsAmountForCalculation + totalRuWordsAmountForCalculation;
+
+		logger.info("CALC STATS | Total: {} words | EN: {} | RU: {}",
+				totalWords,
+				totalEnWordsAmountForCalculation,
+				totalRuWordsAmountForCalculation);
 	}
 }
